078dbac019777e69dedadb3d297a98743a9adf2d
ayodeji1255 <xflimzy4ever@gmail.com>
2025-09-25 18:51:56 +0100

    crash

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,29 @@
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// Create teacher with C experience
+export const cTeacher: Subjects.Teacher = {
+  firstName: "Mary",
+  lastName: "Johnson",
+  experienceTeachingC: 10, // Only C experience
+};
+
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log("React");
+react.setTeacher(cTeacher);
